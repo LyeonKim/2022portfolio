ff64f7d8520c0ff34e37b5a348c2af3d3a9cb819
ChaeLyeonkim <87533823+LyeonKim@users.noreply.github.com>
2022-03-12 16:58:58 +0900

    portfolio page upload
    portfolio page만 업로드, 각 프로젝트 파일은 추후 업로드 예정

---
+++ index.js
@@ -0,0 +1,6 @@
+event1.onmouseover = () => container.style.backgroundColor = '#122548';
+event1.onmouseout = () => container.style.backgroundColor =  '#000';
+event2.onmouseover = () => container.style.backgroundColor = '#20202b';
+event2.onmouseout = () => container.style.backgroundColor =  '#000';
+event3.onmouseover = () => container.style.backgroundColor = '#024126';
+event3.onmouseout = () => container.style.backgroundColor =  '#000';
